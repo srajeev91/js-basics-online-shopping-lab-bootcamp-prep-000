33f3070f0a98c5c16fe9269f93a0052a93328253
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-01-28 01:36:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,8 +70,8 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
     console.log `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-    
-    while ()
-    cart.pop
+    while (cart.length > 0) {
+    cart.pop();
+    }
   }
 }
