73cab94451f1663147fe64ed5e5cefcde563692a
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-01-28 01:31:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,5 +66,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
 }
